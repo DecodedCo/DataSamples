bd13beb88b255738b3836b2c7c8efe866700fe67
Alex Walker <alex@decoded.co>
2015-06-04 17:33:20 +0800

    Create MakeLinkFromTweet.js

---
+++ MakeLinkFromTweet.js
@@ -0,0 +1,14 @@
+function makeLink(text) {
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+ var regex = new RegExp(expression);
+
+  console.log(text.match(regex))
+	if (text.match(regex) )
+ {
+   text = text.replace(text.match(regex), "<a href='"+text.match(regex)+"'></a>")
+ } else {
+   console.log("No match");
+ }
+console.log(text)
+return text
+}
